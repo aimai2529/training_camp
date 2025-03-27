34590c11f841e9df33620e2db04fbf876ff50834
kanayakaisei <kanayakaisei0903@gmail.com>
2025-03-27 15:01:38 +0900

    クリック時変化を追加

--- friendList.js
+++ friendList.js
@@ -3,5 +3,7 @@ const inviteBtn = document.querySelectorAll(".inviteBtn");
 inviteBtn.forEach(button => {
     button.onclick = () => {
         button.innerText = "フレンドに送信しました！";
+        button.style.backgroundColor = "#FF9A42"
+        button.style.color = "#fff"
     };
 });
